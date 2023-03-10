76ba8e108ec82d93d2732d735bee3559719e1781
Saravanan <shravanan560@gmail.com>
2023-03-10 14:46:23 +0530

    commit 001

--- cucumber.js
+++ cucumber.js
@@ -14,6 +14,6 @@ const common = `
     `
 
 module.exports = {
-  //default: `${common} features/**/*.feature`,
-  default: `${common} features/BaiscAuthentication.feature`,
+  default: `${common} features/**/*.feature`,
+  //default: `${common} features/BaiscAuthentication.feature`,
 }
