4fb107b3e140050a48aea7d323ad4128d0f36a36
Saravanan <shravanan560@gmail.com>
2023-03-10 15:30:51 +0530

    commit002

---
+++ cucumber.js
@@ -0,0 +1,18 @@
+const common = `
+    --require setup/assertions.js
+    --require setup/hooks.js
+    --require step-definitions/**/*.step.js
+    --require config
+    --format summary  
+    --require progress-bar
+    --require package.json  
+    --require playwright.config.js  
+    --publish-quiet  
+    --format @cucumber/pretty-formatter
+    --format json:report_cucumber/cucumber-html-report.json
+    --format html:report_cucumber/cucumber-html-report.html     
+    `
+
+module.exports = {
+  default: `${common} features/orangehrm/**/*.feature`,
+  }
