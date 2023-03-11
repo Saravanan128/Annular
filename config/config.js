03c7ce10866a07dc17e57a1cf0cddc65ebb09081
Saravanan <shravanan560@gmail.com>
2023-03-11 12:51:40 +0530

    sdsd

---
+++ config/config.js
@@ -0,0 +1,13 @@
+Object.assign(global, {
+  
+  BASE_URL: 'https://leafground.com/dashboard.xhtml',
+  LETS_CODE_SELECTABLE: 'https://letcode.in/selectable',
+  LETS_CODE_CALENDAR: 'https://letcode.in/forms',
+  BASIC_AUTHEN_URL: 'http://admin:testleaf@leafground.com:8090',
+  IRCTC_URL:'https://www.irctc.com/',
+  ORANGE_HRM_URL:'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+  LETS_CODE_WINDOW:'https://letcode.in/windows'
+
+
+}
+);
